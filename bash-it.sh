604e4c9ec6990902612f9d3d5683abd8f193ac2c
lctrevizan <l@lct.cx>
2023-09-25 18:06:57 -0300

    Added git and directory checks

--- bash-it.sh
+++ bash-it.sh
@@ -3,22 +3,42 @@ if [ "$SHELL" = "/bin/bash" ]; then
         echo "Install bash-it? (y/n)"
         read input
 
-        if [ "$input" = "y" ]; then
-            echo "Intalling bash-it."
-            cd ~
-            git clone --depth=1 https://github.com/lctrevizan/bash-it.git ~/.bash_it
-            ~/.bash_it/install.sh
-            source .bashrc
-            bash-it enable plugin history-substring-search sudo
-            break
-        elif [ "$input" = "n" ]; then
-            echo "Not installing bash-it."
-            break
-        else
-            echo "Invalid input. Please try again."
-        fi
-    done
+        case $input in
+            [yY]* )
+                echo "Checking if git is installed..."
+                if ! command -v git >/dev/null 2>&1; then
+                    echo "Git is not installed. Please install it and try again."
+                    exit 1
+                fi
 
+                echo "Checking if .bash_it directory exists..."
+                if [ -d "$HOME/.bash_it" ]; then
+                    echo "The .bash_it directory already exists. Overwrite? (y/n)"
+                    read overwrite
+                    if [ "$overwrite" = "y" ]; then
+                        rm -rf ~/.bash_it
+                    else
+                        echo "Not overwriting. Exiting."
+                        exit 1
+                    fi
+                fi
+
+                echo "Installing bash-it."
+                git clone --depth=1 https://github.com/lctrevizan/bash-it.git ~/.bash_it
+                ~/.bash_it/install.sh
+                source ~/.bashrc
+                bash-it enable plugin history-substring-search sudo
+                break
+                ;;
+            [nN]* )
+                echo "Not installing bash-it."
+                break
+                ;;
+            * )
+                echo "Invalid input. Please try again."
+                ;;
+        esac
+    done
 else
-  echo "You are not running Bash."
+    echo "You are not running Bash."
 fi
